1047c3c76c9d2e98a8ebe005e673dae1edde56d5
romeolandry <kamgoche@yahoo.com>
2019-09-05 11:16:29 +0200

    init commit

---
+++ src/main/java/com/spb/romeo/sample/main/App.java
@@ -0,0 +1,13 @@
+package com.spb.romeo.sample.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
